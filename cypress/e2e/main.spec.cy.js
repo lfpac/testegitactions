acf58c4f8d0d0818341b14ae593e19c127cf679b
Luiz Felipe Pachêco <araujo13felipe69@gmail.com>
2025-08-18 23:18:39 -0300

    first commit

---
+++ cypress/e2e/main.spec.cy.js
@@ -0,0 +1,16 @@
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('http://localhost:5173/')
+    cy.get('[data-qa="profile-box"]')
+    cy.get('[data-qa="comment-input"]').first().type('Test 123')
+    cy.get('[data-qa="publish-button"]').first().click()
+    cy.get('[data-qa="comment-text"]').contains('Test 123')
+    cy.get('[data-qa="comment-text"]').each(($element) => {
+      if ($element.text() === 'Test 123') {
+        cy.get($element).siblings('header').children('[data-qa="delete-button"]').click()
+      }
+    })
+  })
+})
+
+//pachaslol
